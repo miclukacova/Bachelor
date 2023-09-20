e9f252bd91f51eefef36b539a73a791f101c51c6
miclukacova <miclukacova@gmail.com>
2023-09-20 12:12:08 +0200

    Conformal prediction intervals and beta distribution

--- Beta_distribution.R
+++ Beta_distribution.R
@@ -1,17 +1,21 @@
-n <- seq(10, 1210,200)
+n <- seq(10, 1500, length.out = 6)
 l <- floor((n+1)*0.1)
 
-length(n)
+colors = c('deeppink4','darkcyan','midnightblue', 'darkolivegreen',
+           'lightslateblue','palegreen4','mediumorchid4')
 
-curve(dbeta(x, n[7]+1-l[7],l[7]), 
-      from = 0.5, to = 1.1, 
-      col = 3,
+curve(dbeta(x, n[6]+1-l[6],l[6]), 
+      from = 0.7, to = 1.01, 
+      col =  colors[6],
       main = "Beta distribution",
-      ylab = "Density")
+      ylab = "Density",
+      lwd = 2)
+
+names <- c()
 
 for (i in 1:6){
-  curve(dbeta(x, n[i]+1-l[i],l[i]), from = 0.5, to = 1.1, col = 3, add = TRUE)
+  curve(dbeta(x, n[i]+1-l[i],l[i]), from = 0.7, to = 1.01, col = colors[i], add = TRUE, lwd = 2)
+  names[i] <- c(paste("n = ", n[i]))
 }
 
-?curve
-
+legend(1, 95, legend=names, col=c("red", "blue"))
--- Conformal.R
+++ Conformal.R
@@ -25,39 +25,26 @@ picked = sample(seq_len(nrow(cali_leafs_log)),size = sample_size2)
 test_leafs_log = cali_leafs_log[picked,]
 cali_leafs_log = cali_leafs_log[-picked,]
 
-nrow(train_leafs_log)
-nrow(cali_leafs_log)
-nrow(test_leafs_log)
-
 # The linear model
 
 lm <- lm(Kgp ~ Sc, train_leafs_log)
-
-sigma_hat <- sum(lm$residuals^2)/(nrow(train_leafs_log)-1)
-
-f_hat <- function(x) lm$coefficients[[2]]*x + lm$coefficients[[1]] 
-
-ggplot(leafs_log, aes(x = Sc, y = Kgp)) + 
-  geom_point() + 
-  theme_bw() +
-  xlab('log(Sc)') + 
-  ylab('log(Kgp)')+
-  geom_function(fun = f_hat, colour = "red") +
-  labs(title = "Kgp as function of Sc")
+sd_hat <- sum(lm$residuals^2)/(nrow(train_leafs_log)-1)
+f_hat <- function(x) lm$coefficients[[2]]*x + lm$coefficients[[1]]
 
 # Heuristic notion of uncertainty
 
-s <- sort(abs(f_hat(cali_leafs_log$Sc) - cali_leafs_log$Kgp))/(sigma_hat^(1/2))
+mse <- sort((f_hat(cali_leafs_log$Sc) - cali_leafs_log$Kgp)^2)
 
 quanti <- ceiling((nrow(cali_leafs_log)+1)*(1-0.1))
 
-q_hat <- s[quanti]
+q_hat <- mse[quanti]
 
 # Conformal prediction interval
 
+# Som i noten
+
 upper <- function(x) f_hat(x) + q_hat*sigma_hat^(1/2)
 lower <- function(x) f_hat(x) - q_hat*sigma_hat^(1/2)
-
 
 ggplot(test_leafs_log, aes(x = Sc, y = Kgp)) + 
   geom_point() + 
@@ -69,10 +56,86 @@ ggplot(test_leafs_log, aes(x = Sc, y = Kgp)) +
   geom_function(fun = lower, colour = "blue") +
   labs(title = "Kgp as function of Sc with conformal prediction intervals")
 
-up <- f_hat(cali_leafs_log$Sc) + q_hat*sigma_hat^(1/2)
-low <- f_hat(cali_leafs_log$Sc) - q_hat*sigma_hat^(1/2)
+#Som jeg tænkter (næsten det samme?????)
 
-low <= cali_leafs_log$Kgp
+upper_1 <- function(x) f_hat(x) + sqrt(q_hat)
+lower_1 <- function(x) f_hat(x) - sqrt(q_hat)
+
+ggplot(test_leafs_log, aes(x = Sc, y = Kgp)) + 
+  geom_point() + 
+  theme_bw() +
+  xlab('log(Sc)') + 
+  ylab('log(Kgp)')+
+  geom_function(fun = f_hat, colour = "red") +
+  geom_function(fun = upper_1, colour = "blue") +
+  geom_function(fun = lower_1, colour = "blue") +
+  labs(title = "Kgp as function of Sc with conformal prediction intervals")
+
+#Coverage
+
+up1 <- f_hat(test_leafs_log$Sc) + sqrt(q_hat)
+low1 <- f_hat(test_leafs_log$Sc) - sqrt(q_hat)
+up <- f_hat(test_leafs_log$Sc) + q_hat*sigma_hat^(1/2)
+low <- f_hat(test_leafs_log$Sc) - q_hat*sigma_hat^(1/2)
+
+mean(low1 <= test_leafs_log$Kgp & test_leafs_log$Kgp  <= up1)
+mean(low <= test_leafs_log$Kgp & test_leafs_log$Kgp  <= up)
+
+#cv
+
+cv_cov <- function(data, k, alpha) {
+  cov <- c()
+  n <- nrow(data)
+  group <- sample(rep(1:k, length.out = n))
+  for (i in (1:k)){
+    #Fit model
+    if (i != k){
+      data_cv <- data[group != i & group != (i+1), ]
+      n_cv <- nrow(data_cv)
+      n_cali <- nrow(data[group == i, ])
+      
+      lm_cv <- lm(Kgp ~ Sc, data = data_cv)
+      f_hat <- function(x) lm_cv$coefficients[[2]]*x + lm_cv$coefficients[[1]]
+      
+      # Heuristic notion of uncertainty
+      mse <- sort((f_hat(data[group == i, ]$Sc) - data[group == i, ]$Kgp)^2)
+      quanti <- ceiling((n_cali+1)*(1-alpha))
+      q_hat <- mse[quanti]
+      print(q_hat)
+      
+      # Coverage
+      up_cv <- f_hat(data[group == (i+1), ]$Sc) + sqrt(q_hat)
+      low_cv <- f_hat(data[group == (i+1), ]$Sc) - sqrt(q_hat)
+      
+      cov[i] <- mean(low_cv <= data[group == (i+1), ]$Kgp & data[group == (i+1), ]$Kgp  <= up_cv)
+    }
+    else {
+      data_cv <- data[group != k & group != 1, ]
+      n_cv <- nrow(data_cv)
+      n_cali <- nrow(data[group == k, ])
+      
+      lm_cv <- lm(Kgp ~ Sc, data = data_cv)
+      f_hat <- function(x) lm_cv$coefficients[[2]]*x + lm_cv$coefficients[[1]]
+      
+      # Heuristic notion of uncertainty
+      
+      mse <- sort((f_hat(data[group == k, ]$Sc) - data[group == k, ]$Kgp)^2)
+      quanti <- ceiling((n_cali+1)*(1-alpha))
+      q_hat <- mse[quanti]
+      
+      # Conformal prediction interval
+      up_cv <- f_hat(data[group == 1, ]$Sc) + sqrt(q_hat)
+      low_cv <- f_hat(data[group == 1, ]$Sc) - sqrt(q_hat)
+      
+      # Coverage
+      cov[i] <- mean(low_cv <= data[group == 1, ]$Kgp & data[group == 1, ]$Kgp  <= up_cv)
+    }
+  }
+  return(tibble("Coverage" = cov))
+}
+
+cv_cov(leafs_log,10,0.1)
+
 
 
 
