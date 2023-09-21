f00a7e7bc11356302b77314e34ce80493945095c
DinaGyberg <vbz248@alumni.ku.dk>
2023-09-21 21:25:49 +0200

    Graf og indledende quantile forest

--- Forest_quantileregression.R
+++ Forest_quantileregression.R
@@ -1,2 +1,19 @@
-library(randomForestSRC)
+leafs_log <- read.csv('Data/leafs_log.csv')
+roots_log <- read.csv('Data/roots_log.csv')
+wood_log <- read.csv('Data/wood_log.csv')
+
+library(tidyverse)
+library(readr)
+library(infer)
+library(randomForest)
+library(RColorBrewer)
 library(quantregForest)
+
+x <- data.frame(leafs_log[1])
+y <- data.frame(leafs_log[2])
+
+quantregForest(x, y, keep.inbag = FALSE)
+
+?randomForest
+
+
