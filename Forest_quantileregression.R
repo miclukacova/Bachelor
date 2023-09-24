0749f19e023485f09eb6e1b01329d45b83dc9743
DinaGyberg <vbz248@alumni.ku.dk>
2023-09-24 15:46:46 +0200

    for træ

--- Forest_quantileregression.R
+++ Forest_quantileregression.R
@@ -65,3 +65,40 @@ ggplot(plot_data, aes(x = Sc)) +
        x = "Crown size")+
   theme_bw()+
 scale_color_manual(values = colors)
+
+
+#Med wood i stedet:
+wood <- read.csv('Data/wood.csv')
+sample_size_wood = floor(0.8*nrow(wood))
+set.seed(777)
+picked_wood = sample(seq_len(nrow(wood)),size = sample_size_wood)
+
+train_wood_x = data.frame(Sc = wood[picked_wood,1])
+train_wood_y = wood[picked_wood,2]
+test_wood_x = data.frame(Sc=wood[-picked_wood,1])
+test_wood_y = wood[-picked_wood,2]
+
+qrf <- quantregForest(x = train_wood_x, y =train_wood_y)
+plot(qrf)
+
+conditionalQuantiles <- predict(qrf, test_wood_x)
+print(conditionalQuantiles[1:4,])
+
+plot_data <- data.frame(test_wood_x, conditionalQuantiles)
+head(plot_data)
+
+plot_data <- arrange(plot_data, Sc)
+colors <- c("0.1" = "hotpink", "0.5" = "darkolivegreen", "0.9" = "darkolivegreen2")
+
+ggplot(plot_data, aes(x = Sc)) +
+  geom_point(aes(y=quantile..0.1, color = "0.1")) +
+  geom_point(aes(y=quantile..0.5, color = "0.5")) +
+  geom_point(aes(y=quantile..0.9, color = "0.9" )) +
+  geom_smooth(aes(y=quantile..0.1, color = "0.1"), se = F)+
+  geom_smooth(aes(y=quantile..0.5, color = "0.5"), se = F)+
+  geom_smooth(aes(y=quantile..0.9, color = "0.9"), se = F)+
+  labs(color = "quantile",
+       y = "Quantiles",
+       x = "Crown size")+
+  theme_bw()+
+  scale_color_manual(values = colors)
