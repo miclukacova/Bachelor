29b72c1cf28668f7d66db975a86714425544e685
miclukacova <miclukacova@gmail.com>
2023-10-12 15:54:06 +0200

    noget som ikke helt er færdigt

--- pred_int_logn.R
+++ pred_int_logn.R
@@ -39,6 +39,272 @@ var_hat <- c(var(lm_leafs_log$residuals),
              var(lm_roots_log$residuals),
              var(lm_wood_log$residuals))
 
+#Mean of the distribution on log scale
+
+mean_l_log <- function(x) lm_leafs_log$coefficients[[1]]+ lm_leafs_log$coefficients[[2]]*log(x)
+mean_w_log <- function(x) lm_wood_log$coefficients[[1]]+ lm_wood_log$coefficients[[2]]*log(x)
+mean_r_log <- function(x) lm_roots_log$coefficients[[1]]+ lm_roots_log$coefficients[[2]]*log(x)
+mean_l <- function(x) exp(lm_leafs_log$coefficients[[1]])*x^lm_leafs_log$coefficients[[2]]
+mean_w <- function(x) exp(lm_wood_log$coefficients[[1]])*x^lm_wood_log$coefficients[[2]]
+mean_r <- function(x) exp(lm_roots_log$coefficients[[1]])*x^lm_roots_log$coefficients[[2]]
+
+# Quantiles
+
+quant_low_l <- function(x) qlnorm(0.05, meanlog = mean_l_log(x), sdlog = var_hat[1]) 
+quant_up_l <- function(x) qlnorm(0.95, meanlog = mean_l_log(x), sdlog = var_hat[1]) 
+
+quant_low_w <- function(x) qlnorm(0.05, meanlog = mean_w_log(x), sdlog = var_hat[2]) 
+quant_up_w <- function(x) qlnorm(0.95, meanlog = mean_w_log(x), sdlog = var_hat[2])
+
+quant_low_r <- function(x) qlnorm(0.05, meanlog = mean_r_log(x), sdlog = var_hat[3]) 
+quant_up_r <- function(x) qlnorm(0.95, meanlog = mean_r_log(x), sdlog = var_hat[3])
+
+#On test set 
+
+
+test_leafs_plot <- leafs_test %>%
+  mutate(Indicator = if_else((quant_low_l(Sc) <= Kgp)&
+                               (Kgp <= quant_up_l(Sc)),"in", "out"))
+
+test_wood_plot <- wood_test %>%
+  mutate(Indicator = if_else((quant_low_w(Sc) <= Kgp)&
+                               (Kgp <= quant_up_w(Sc)),"in", "out"))
+
+test_roots_plot <- roots_test %>%
+  mutate(Indicator = if_else((quant_low_l(Sc) <= Kgp)&
+                               (Kgp <= quant_up_l(Sc)),"in", "out"))
+
+#Coverage
+
+coverage <- function(data, upper, lower){
+  mean(lower(data$Sc) <= data$Kgp &upper(data$Sc) >= data$Kgp)
+}
+
+a <- coverage(leafs_test, quant_up_l, quant_low_l)
+b <- coverage(wood_test, quant_up_w, quant_low_w)
+c <- coverage(roots_test, quant_up_r, quant_low_r)
+
+xtable(tibble("Data" = c("Leafs", "Wood", "Roots"), "Coverage" = c(a,b,c)), type = latex)
+
+#Plot with prediction intervals real scale
+
+color <- c("in" = "darkolivegreen", "out" = "darkolivegreen3")
+
+ggplot(test_leafs_plot, aes(x = Sc, y = Kgp)) + 
+  geom_point(aes(color = Indicator)) + 
+  theme_bw() +
+  xlab('Sc') + 
+  ylab('Kgp')+
+  geom_function(fun = mean_l, colour = "hotpink1") +
+  geom_function(fun = quant_low_l, colour = "hotpink4") +
+  geom_function(fun = quant_up_l, colour = "hotpink4") +
+  labs(title = "Leafs")+
+  scale_color_manual(values = color)
+
+ggplot(test_wood_plot, aes(x = Sc, y = Kgp)) + 
+  geom_point(aes(color = Indicator)) + 
+  theme_bw() +
+  xlab('Sc') + 
+  ylab('Kgp')+
+  geom_function(fun = mean_w, colour = "hotpink1") +
+  geom_function(fun = quant_low_w, colour = "hotpink4") +
+  geom_function(fun = quant_up_w, colour = "hotpink4") +
+  labs(title = "Wood")+
+  scale_color_manual(values = color)
+
+ggplot(test_roots_plot, aes(x = Sc, y = Kgp)) + 
+  geom_point(aes(color = Indicator)) + 
+  theme_bw() +
+  xlab('Sc') + 
+  ylab('Kgp')+
+  geom_function(fun = mean_r, colour = "hotpink1") +
+  geom_function(fun = quant_low_r, colour = "hotpink4") +
+  geom_function(fun = quant_up_r, colour = "hotpink4") +
+  labs(title = "Roots")+
+  scale_color_manual(values = color)
+
+
+#Random split to assess coverage: 
+
+rs_cov <- function(data, k, alpha) {
+  cov <- c()
+  n <- nrow(data)
+  sample_size <- floor(0.8*n)
+  
+  for (i in (1:k)){
+    # Test and train
+    picked_rs <- sample(n,size = sample_size)
+    train_rs = data[picked_rs,]
+    test_rs = data[-picked_rs,]
+    
+    # Fit model
+    lm_rs <- lm(Kgp ~ Sc, data = train_rs)
+    sd_hat <- sqrt(sum(lm_rs$residuals^2)/(sample_size-1))
+    f_hat <- function(x) lm_rs$coefficients[[2]]*x + lm_rs$coefficients[[1]] 
+    
+    # Quantiles
+    upper <- function(x) {
+      f_hat(x) - qt(alpha/2, sample_size-1)*sqrt(x^2/sum(train_rs$Sc^2)+1)*sd_hat
+    }
+    
+    lower <- function(x) {
+      f_hat(x) - qt(1-alpha/2, sample_size-1)*sqrt(x^2/sum(train_rs$Sc^2)+1)*sd_hat
+    }
+    
+    #Definere
+    cov[i] <- mean(lower(test_rs$Sc) <= test_rs$Kgp 
+                   &upper(test_rs$Sc) >= test_rs$Kgp)
+  }
+  return(tibble("Coverage" = cov))
+}
+
+##Checking for correct coverage
+#
+#set.seed(4)
+#a <- rs_cov(leafs_log, 30, 0.1)
+#b <- rs_cov(wood_log, 30, 0.1)
+#c <- rs_cov(roots_log, 30,0.1)
+#
+##Mean coverage:
+#mean_a <- mean(a$Coverage)
+#mean_b <- mean(b$Coverage)
+#mean_c <- mean(c$Coverage) 
+#
+#xtable(tibble(Data = c("Leafs", "Wood", "Roots"), 
+#              "Mean coverage" =c(mean_a, mean_b, mean_c)), type = latex)
+#
+#a %>%
+#  ggplot() +
+#  geom_histogram(aes(x = Coverage, y = ..density..), color = "white", 
+#                 fill = "darkolivegreen3", bins = 30)+
+#  geom_vline(xintercept = 0.9, color = "hotpink") +
+#  xlim(0,1)+
+#  theme_bw()+
+#  labs(title = "Foliage")
+#
+#b %>%
+#  ggplot() +
+#  geom_histogram(aes(x = Coverage, y = ..density..), color = "white", 
+#                 fill = "darkolivegreen3", bins = 30)+
+#  geom_vline(xintercept = 0.9, color = "hotpink") +
+#  theme_bw()+
+#  xlim(c(0.5,1))+
+#  labs(title = "Wood")
+#
+#c %>%
+#  ggplot() +
+#  geom_histogram(aes(x = Coverage, y = ..density..), color = "white", 
+#                 fill = "darkolivegreen3", bins = 30)+
+#  geom_vline(xintercept = 0.9, color = "hotpink") +
+#  theme_bw()+
+#  xlim(c(0,1))+
+#  labs(title = "Roots")
+#
+#
+##Checking for conditional coverage----------------------------------------------
+#
+#coverage <- function(data, upper, lower){
+#  mean(lower(data$Sc) <= data$Kgp & upper(data$Sc) >= data$Kgp)
+#}
+#
+#cond_cov <- function(data, upper, lower, num_bins){
+#  #Bins
+#  bins <- sort(data$Sc)[seq(1,num_bins)*floor(nrow(data)/num_bins)]
+#  indi <- c()
+#  
+#  for (i in (1:nrow(data))){
+#    x <- data$Sc[i]
+#    j_0 <- - Inf
+#    for (j in (1:num_bins)){
+#      if (j_0 < x & x <= bins[j]){
+#        indi[i] <- j
+#        j_0 <- bins[j]
+#      }
+#    }
+#  }
+#  cond_cov_data <- cbind(data, indi = indi)
+#  
+#  #Conditional Coverage
+#  
+#  cond_cov_vec <- c()
+#  
+#  for (i in cond_cov_data$indi){
+#    zz <- cond_cov_data %>%
+#      filter(indi == i)
+#    
+#    cond_cov_vec[i] <- coverage(zz, upper, lower)
+#  }
+#  
+#  j_0 <- - Inf
+#  bins2 <- c()
+#  for (i in (1:num_bins)){
+#    bins2[i] <- paste("[", j_0, ",", round(bins[i],2), "]")
+#    j_0 <- round(bins[i],2)
+#  }
+#  
+#  return(tibble(Bin = bins2, "Conditional coverage" = cond_cov_vec))
+#}
+#
+#a <- cond_cov(test_leafs_log, upper_leafs, lower_leafs, 5)
+#b <- cond_cov(test_wood_log, upper_wood, lower_wood, 5)
+#
+#nrow(test_wood_log)
+#
+#xtable(cbind(a,b), type = latex)
+#xtable(b, type = latex)
+#
+#bins <- sort(test_leafs_log$Sc)[seq(1,5)*floor(nrow(test_leafs_log)/5)]
+#
+#ggplot(test_leafs_log_plot, aes(x = Sc, y = Kgp)) + 
+#  geom_point(aes(color = Indicator)) + 
+#  theme_bw() +
+#  xlab('log(Sc)') + 
+#  ylab('log(Kgp)')+
+#  geom_function(fun = f_hat_leafs, colour = "hotpink1") +
+#  geom_function(fun = upper_leafs, colour = "hotpink4") +
+#  geom_function(fun = lower_leafs, colour = "hotpink4") +
+#  geom_vline(xintercept = bins[1], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[2], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[3], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[4], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[5], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  labs(title = "Leafs")+
+#  scale_color_manual(values = color)
+#
+#bins <- sort(test_wood_log$Sc)[seq(1,5)*floor(nrow(test_wood_log)/5)]
+#
+#ggplot(test_wood_log_plot, aes(x = Sc, y = Kgp)) + 
+#  geom_point(aes(color = Indicator)) + 
+#  theme_bw() +
+#  xlab('log(Sc)') + 
+#  ylab('log(Kgp)')+
+#  geom_function(fun = f_hat_wood, colour = "hotpink1") +
+#  geom_function(fun = upper_wood, colour = "hotpink4") +
+#  geom_function(fun = lower_wood, colour = "hotpink4") +
+#  geom_vline(xintercept = bins[1], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[2], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[3], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[4], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  geom_vline(xintercept = bins[5], linetype = "dotted", linewidth = 0.3, color = "hotpink3")+
+#  labs(title = "Wood")+
+#  scale_color_manual(values = color)
+
+
+## Min metode - skal rettes til 
+
+
+#Estimeret middelværdi af Y
+
+hat_y_leafs <- function(x) exp(hat_beta[1])* x^hat_alpha[1]*exp(var_hat[1]/2)
+hat_y_roots <- function(x) exp(hat_beta[2])* x^hat_alpha[2]*exp(var_hat[2]/2)
+hat_y_wood <- function(x) exp(hat_beta[3])* x^hat_alpha[3]*exp(var_hat[3]/2)
+
+#Estimeret varians af Y
+
+var_y_hat_leafs <- function(x) exp(2*hat_y_leafs(x) + var_hat[1])*(exp(var_hat[1])-1) 
+var_y_y_hat_wood <- function(x) exp(2*hat_y_wood(x) + var_hat[2])*(exp(var_hat[2])-1)  
+var_y_y_hat_roots <- function(x) exp(2*hat_y_roots(x) + var_hat[3])*(exp(var_hat[3])-1)  
+
 # hat log Y som funktion af log x
 
 hat_log_y_leafs <- function(x) hat_beta[1] + hat_alpha[1]*x
@@ -90,16 +356,4 @@ u <- pred_int_up_log(leafs_test$Sc)
 color <- c("darkolivegreen1", "darkolivegreen4")
 #aes(color = Indicator)
 
-ggplot(data.frame(leafs_test, l=l, u=u), aes(x = Sc, y = Kgp)) + 
-  geom_point() + 
-  geom_point(aes(y=l)+
-  geom_point(aes(y=u)+
-  theme_bw() +
-  xlab('Sc') + 
-  ylab('Kgp')+
-  #geom_function(fun = hat_y_leafs, colour = "hotpink1") +
-  #geom_function(fun = pred_int_low_log, colour = "hotpink4") +
-  geom_function(fun = pred_int_up_log, colour = "hotpink4") +
-  labs(title = "Leafs")
-#+scale_color_manual(values = color)
 
