69dbe3c896f456e5d81aa6051a4640daf357474d
DinaGyberg <vbz248@alumni.ku.dk>
2023-12-07 12:00:44 +0100

    Clean up and plots

--- Random_forest_model.R
+++ Random_forest_model.R
@@ -14,44 +14,14 @@ wood<- read.csv('Data/wood.csv')
 roots <- read.csv('Data/roots.csv')
 
 
+#---------------Fitting the QRF and calculating predictions + intervals:
 
-##############################################################
-#Implementering af random forest modellen, i sammenligning med QRF for at se hvad der foregår:
+#Defining the nodegrids to be searched:
+node_grid_l <-c(5,70,100,150)
+node_grid_w <-c(5,70,130)
+node_grid_r <-c(5)
 
-#Fitting the model
-rf_leafs_model <- randomForest(x = train_leafs_x, y = train_leafs_y, nodesize = 100)
-
-#Checking number of splits
-for (i in 1:500){
-  tree = data.frame(getTree(rf_leafs_model, k = i))
-  endnodes = tree[tree$status == -1,]
-  leng <- nrow(endnodes)
-  size <- 900/leng
-  print(leng)
-}
-
-#Using it to predict:
-rf_leafs_pred <- predict(rf_leafs_model, newdata = test_leafs_x)
-
-#Function for creating the dataframe to be plotted:
-plot_data <- function(pred, obs, Sc){
-  plot_data <- data.frame(pred, observed = obs, Sc = Sc)
-  }
-
-plot_leafs <- plot_data(rf_leafs_pred, test_leafs_y, test_leafs_x)
-
-ggplot(plot_leafs) + 
-  geom_point(aes(x = Sc, y = observed), color = 'darkolivegreen4', size = 1.5) +
-  geom_point(aes(x = Sc, y = pred), color = 'hotpink4', size = 1.5) +
-  theme_bw() +
-  xlab('Sc') + 
-  ylab('Kgp')+
-  labs(title = "Leafs")
-
-#Random Forest
-
-node_grid <-c(5,100,150)
-
+#Function performing CV to choose minimal nodesize
 node_size_choose <- function(data, k = 3, node_grid) {
   set.seed(4)
   MSE_ns <- c()
@@ -75,9 +45,10 @@ node_size_choose <- function(data, k = 3, node_grid) {
 }
 
 
-loo_rf <- function(data) {
+#LOOCV-function that produces predictions for both mean and intervals:
+loo_rf <- function(data, node_grid) {
   set.seed(777)
-  pred <- c() ; obs <- c() ; q0.1 <- c() ; q0.9 <- c() ;mean_cv <- c()
+  pred <- c() ; obs <- c() ; q0.1 <- c() ; q0.9 <- c() ;mean_cv <- c(); node_size <- c()
   for (i in (1:nrow(data))){
     print(i)
     #choose nodesize
@@ -100,78 +71,45 @@ loo_rf <- function(data) {
     q0.1    <- append(q0.1, conditionalQuantiles_cv[,1])
     q0.9    <- append(q0.9, conditionalQuantiles_cv[,2])
     mean_cv <- append(mean_cv, conditionalMean_cv)
+    node_size <- append(node_size, nodesize)
   }
-  return("obs_int" = data.frame(observed = obs, quantile..0.1 = q0.1,quantile..0.9 = q0.9, pred=pred, mean_cv = mean_cv))
+  return("obs_int" = data.frame(Kgp = obs, quantile..0.1 = q0.1,quantile..0.9 = q0.9, Sc=pred, predicted_value = mean_cv, nodesize = nodesize))
 }
 
-leafs_pred <- loo_rf(leafs)
-wood_pred <- loo_rf(wood)
-roots_pred <- loo_rf(roots)
 
-#Constructing data to be plotted:
+leafs_pred <- loo_rf(leafs, node_grid_l)
+wood_pred <- loo_rf(wood, node_grid_w)
+roots_pred <- loo_rf(roots, node_grid_r)
 
-plot_f <- function(data, title){
-  data <- data.frame(data, x = leafs$Sc)
-  ggplot(data)+
-    geom_point(aes(x = obs, y = Sc), color = 'darkolivegreen')+
-    #geom_point(aes(x = obs, y = mean_pred), color = 'hotpink3')+
-    xlab("Sc")+
-    ylab("Kgp")+
-    ggtitle(title)+
-    theme_bw()
-}
+#Skriver det til en CSV så det er nemmere at arbejde med
 
-plot_f(leafs_pred, "Leafs")
-plot_f(wood_pred, "Wood")
-plot_f(roots_pred, "Roots")
 
-cv <- function(data, k) {
-  set.seed(777)
-  n <- nrow(data)
-  group <- sample(rep(1: k, length.out = n))
-  pred <- c()
-  obs <- c()
-  q0.1 <- c()
-  q0.9 <- c()
-  for (i in (1:k)){
-    #Fit model
-    train_x <- data.frame(Sc = data[group != i,1])
-    train_y <- data[group != i,2]
-    test_x <- data.frame(Sc=data[group == i,1])
-    test_y <- data[group == i,2]
-    qrf_cv <- quantregForest(x = train_x, y =train_y, nodesize = 76)
-    
-    #Getting quantiles
-    conditionalQuantiles_cv <- predict(qrf_cv, test_x, what = c(0.05,0.95))
-    
-    #Creating vectors with observations and quantiles
-    if (i == 1) {
-      pred <- c(data[group == i,1])
-      obs <- c(test_y)
-      q0.1 <- c(conditionalQuantiles_cv[,1])
-      q0.9 <- c(conditionalQuantiles_cv[,2])
-    }
-    else {
-      pred <- c(pred, data[group == i,1])
-      obs <- c(obs, test_y)
-      q0.1 <- c(q0.1, conditionalQuantiles_cv[,1])
-      q0.9 <- c(q0.9, conditionalQuantiles_cv[,2])
-    }
-  }
-  return("obs_int" = data.frame(observed = obs, quantile..0.1 = q0.1,quantile..0.9 = q0.9, pred=pred))
-}
+#Til Michaelas computer:
+write.csv(leafs_pred, "/Users/michaelalukacova/Bachelor1/Data/QRF_intervals_leafs.csv", row.names=F)
+write.csv(wood_pred, "/Users/michaelalukacova/Bachelor1/Data/QRF_intervals_wood.csv", row.names=F)
+write.csv(roots_pred, "/Users/michaelalukacova/Bachelor1/Data/QRF_intervals_roots.csv", row.names=F)
 
-#########################Real one###########################333
-#You need to run the code in document Forest_plots
 
-#Plot
+#Til Dinas computer
+write.csv(leafs_pred, "C:/Users/Bruger/OneDrive - University of Copenhagen/4. år/Bachelor/Bachelor1/Data/QRF_intervals_leafs.csv", row.names=F)
+write.csv(wood_pred, "C:/Users/Bruger/OneDrive - University of Copenhagen/4. år/Bachelor/Bachelor1/Data/QRF_intervals_wood.csv", row.names=F)
+write.csv(roots_pred, "C:/Users/Bruger/OneDrive - University of Copenhagen/4. år/Bachelor/Bachelor1/Data/QRF_intervals_roots.csv", row.names=F)
+
+
+#---------Indlæsning af predicted values and prediction intervals:--------
+#OBS man skal have CSV-filerne liggende før det giver mening:
+
+leafs_pred <- read.csv("Data/QRF_intervals_leafs.csv")
+wood_pred <- read.csv("Data/QRF_intervals_wood.csv")
+roots_pred <- read.csv("Data/QRF_intervals_roots.csv")
+
+#-----------------MODEL--------------------------------------------
+#Defining function to plot the prediction of the mean:
 Sc_plot <- function(data,title){
   plot_data <- data.frame(data)
-  plot_data <- plot_data %>%
-    mutate(indicator = if_else((quantile..0.1 <= observed)&(observed <= quantile..0.9),"in", "out"))
   #Plot
-  ggplot(plot_data, aes(x = pred)) +
-    geom_point(aes(y=observed), color = "darkolivegreen", alpha=0.5, fill = "darkolivegreen3") +
+  ggplot(plot_data, aes(x = Sc)) +
+    geom_point(aes(y=Kgp), color = "darkolivegreen", alpha=0.5, fill = "darkolivegreen3") +
     geom_point(aes(y=mean_cv), color = 'hotpink3', fill = "hotpink2") +
     labs(title = title)+
     xlab("Sc")+
@@ -179,21 +117,74 @@ Sc_plot <- function(data,title){
     theme_bw()
 }
 
-Sc_plot(cv_intervals_leafs, "Leafs")
-Sc_plot(cv_intervals_wood, "Wood")
-Sc_plot(cv_intervals_roots, "Roots")
+Sc_plot(leafs_pred, "Leafs")
+Sc_plot(wood_pred, "Wood")
+Sc_plot(roots_pred, "Roots")
 
-roll_cov <- function(pred_int, alpha = 0.2, bin_size = 50, title){
-  roll_cov <- c()
+
+#Calculating MSE and bias:
+
+
+#--------------PREDICTION INTERVALS------------------------------
+
+#Defining function for plotting prediction intervals:
+plot_maker <- function(pred_int, title){
   
-leafs_a <- leafs %>%
-    arrange(Sc)
+  pred_plot <- pred_int %>%
+    mutate(Indicator = if_else((quantile..0.1 <= Kgp)&(Kgp <= quantile..0.9),"in", "out"))
+  color <- c("in" = "darkolivegreen", "out" = "darkolivegreen3")
+  
+  ggplot(pred_plot, aes(x = Sc, y = Kgp)) +
+    geom_point(aes(x = Sc, y = Kgp, color = Indicator), size = 0.8, alpha = 0.7) + 
+    geom_point(aes(x = Sc, y = quantile..0.9), color = "hotpink", size = 0.6, alpha = 0.7) + 
+    geom_point(aes(x = Sc, y = quantile..0.1), color = "hotpink", size = 0.6, alpha = 0.7) +
+    theme_bw() +
+    xlab('Sc') + 
+    ylab('Kgp')+
+    labs(title = title)+
+    scale_color_manual(values = color)+ 
+    theme(legend.position = "none")
+}
 
-leafs_a[800:900,]
+plot_maker(leafs_pred, "Leafs")
+plot_maker(wood_pred, "Wood")
+plot_maker(roots_pred, "Roots")
+
+#.------------------------------Initial investigation.----------------
+#Implementering af random forest modellen, i sammenligning med QRF for at se hvad der foregår:
+
+#Fitting the model
+rf_leafs_model <- randomForest(x = train_leafs_x, y = train_leafs_y, nodesize = 100)
+
+#Checking number of splits
+for (i in 1:500){
+  tree = data.frame(getTree(rf_leafs_model, k = i))
+  endnodes = tree[tree$status == -1,]
+  leng <- nrow(endnodes)
+  size <- 900/leng
+  print(leng)
+}
+
+#Using it to predict:
+rf_leafs_pred <- predict(rf_leafs_model, newdata = test_leafs_x)
+
+#Function for creating the dataframe to be plotted:
+plot_data <- function(pred, obs, Sc){
+  plot_data <- data.frame(pred, observed = obs, Sc = Sc)
+}
+
+plot_leafs <- plot_data(rf_leafs_pred, test_leafs_y, test_leafs_x)
+
+ggplot(plot_leafs) + 
+  geom_point(aes(x = Sc, y = observed), color = 'darkolivegreen4', size = 1.5) +
+  geom_point(aes(x = Sc, y = pred), color = 'hotpink4', size = 1.5) +
+  theme_bw() +
+  xlab('Sc') + 
+  ylab('Kgp')+
+  labs(title = "Leafs")
 
 
-
-##############fFAKE MODEL######################################
+##-------------------FAKE MODEL--------------------------------
 #En fake model for sjov
 
 #Fitting the model
