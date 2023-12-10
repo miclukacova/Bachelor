ca4fa985fab8f0022095e5ee2153236f62acfcbe
miclukacova <miclukacova@gmail.com>
2023-12-10 13:56:29 +0100

    boot

--- Bootstrap_true.R
+++ Bootstrap_true.R
@@ -12,22 +12,73 @@ set.seed(777)
 leafs <- read.csv('Data/leafs.csv')
 wood<- read.csv('Data/wood.csv')
 roots <- read.csv('Data/roots.csv')
+
+
 ##############################################################################
 
 
-#----------------------Creating Bootstrap function---------------------------
-
-#Model skal spytte et alpha og et beta estimat ud.
+#----------------------logOLS---------------------------
   
-boot_leafs <- bootstrap_loo(model_logolsB, leafs, 100, alpha = 0.2)
-boot_wood <- bootstrap_loo(model_logolsB, wood, 100, alpha = 0.2)
-boot_roots <- bootstrap_loo(model_logolsB, roots, 100, alpha = 0.2) 
+#boot_leafs <- bootstrap_loo(model_logols, leafs, 1000, alpha = 0.2)
+#boot_wood <- bootstrap_loo(model_logols, wood, 1000, alpha = 0.2)
+#boot_roots <- bootstrap_loo(model_logols, roots, 1000, alpha = 0.2) 
+
+#write.csv(boot_leafs, "/Users/michaelalukacova/Bachelor1/Data/boot_leafs_logols.csv", row.names=F)
+#write.csv(boot_wood, "/Users/michaelalukacova/Bachelor1/Data/boot_wood_logols.csv", row.names=F)
+#write.csv(boot_roots, "/Users/michaelalukacova/Bachelor1/Data/boot_roots_logols.csv", row.names=F)
+
+boot_leafs <- read.csv('Data/boot_leafs_logols.csv')
+boot_wood <- read.csv('Data/boot_wood_logols.csv')
+boot_roots <- read.csv('Data/boot_roots_logols.csv')
 
 plot_maker(boot_leafs, "Leafs")
 plot_maker(boot_wood, "Wood")
 plot_maker(boot_roots, "Roots")
 
 coverage(boot_leafs)
+coverage(boot_wood)
+coverage(boot_roots)
+
+#Distribution of coverage
+
+boot_leafs_rs <- rs_cov_boot(data = leafs[1:100,], k = 10, alpha = 0.2, model = model_logolsB)
+boot_wood_rs <- rs_cov_boot(data = wood, k = 10, alpha = 0.2, model = model_logolsB)
+boot_roots_rs <- rs_cov_boot(data = roots, k = 10, alpha = 0.2, model = model_logolsB)
+
+rs_plot_maker(boot_leafs_rs, "Leafs", alpha = 0.2)
+rs_plot_maker(boot_wood_rs, "Wood", alpha = 0.2)
+rs_plot_maker(boot_roots_rs, "Roots", alpha = 0.2)
+
+#Conditional coverage
+
+roll_cov_boot(boot_leafs, alpha = 0.2, bin_size = 50, "Leafs")
+roll_cov_boot(boot_wood, alpha = 0.2, bin_size = 50, "Wood")
+roll_cov_boot(boot_roots, alpha = 0.2, bin_size = 5, "Roots")
+
+
+#----------------------logOLSB---------------------------
+
+
+#----------------------logOLS---------------------------
+
+boot_leafs <- bootstrap_loo(model_logolsB, leafs, 1000, alpha = 0.2)
+boot_wood <- bootstrap_loo(model_logolsB, wood, 1000, alpha = 0.2)
+boot_roots <- bootstrap_loo(model_logolsB, roots, 1000, alpha = 0.2) 
+
+write.csv(boot_leafs, "/Users/michaelalukacova/Bachelor1/Data/boot_leafs_logols.csv", row.names=F)
+write.csv(boot_wood, "/Users/michaelalukacova/Bachelor1/Data/boot_wood_logols.csv", row.names=F)
+write.csv(boot_roots, "/Users/michaelalukacova/Bachelor1/Data/boot_roots_logols.csv", row.names=F)
+
+boot_leafs <- read.csv('Data/boot_leafs_logols.csv')
+boot_wood <- read.csv('Data/boot_wood_logols.csv')
+boot_roots <- read.csv('Data/boot_roots_logols.csv')
+
+plot_maker(boot_leafs, "Leafs")
+plot_maker(boot_wood, "Wood")
+plot_maker(boot_roots, "Roots")
+
+coverage(boot_leafs)
+coverage(boot_wood)
 coverage(boot_roots)
 
 #Distribution of coverage
@@ -46,5 +97,20 @@ roll_cov(boot_leafs, alpha = 0.2, bin_size = 50, "Leafs")
 roll_cov(boot_leafs, alpha = 0.2, bin_size = 50, "Leafs")
 
 
+#----------------------NLR---------------------------
 
+starting_point_leafs <- c(0.61,0.81)
+starting_point_wood <- c(5.51,0.73)
+starting_point_roots <- c(3.66,0.1)
 
+model_NLR_leafs <- function(data) model_NLR(data, starting_point_leafs)
+model_NLR_wood <- function(data) model_NLR(data, starting_point_wood)
+model_NLR_roots <- function(data) model_NLR(data, starting_point_roots)
+
+boot_leafs <- bootstrap_loo(model_NLR_leafs, leafs, 1000, alpha = 0.2)
+boot_wood <- bootstrap_loo(model_NLR_wood, wood, 1000, alpha = 0.2)
+boot_roots <- bootstrap_loo(model_NLR_roots, wood, 1000, alpha = 0.2)
+
+plot_maker(boot_leafs, "Leafs")
+plot_maker(boot_wood, "Wood")
+plot_maker(boot_roots, "Roots")
--- Functions.R
+++ Functions.R
@@ -1,13 +1,31 @@
+################################################################################
 #Bootstrap functions
+################################################################################
 
-coverage <- function(pred_int) {
-  mean((pred_int$Low <= pred_int$Kgp) & (pred_int$Kgp <= pred_int$High))
+#Model skal spytte et alpha og et beta estimat ud.
+
+model_logols <- function(data){
+  model <- lm(log(Kgp) ~ log(Sc), data)
+  return(list(beta = exp(model$coef[[1]]), alpha = model$coef[[2]]))
 }
 
 model_logolsB <- function(data){
   model <- lm(log(Kgp) ~ log(Sc), data)
+  bias_term <- exp(var(model$residuals)/2)
+  model$coef[[1]] <- exp(model$coef[[1]])*bias_term
   return(list(beta = model$coef[[1]], alpha = model$coef[[2]]))
 }
+
+MSE_NLR <- function(par, data){
+  with(data, sum((Kgp-par[1]*Sc^par[2])^2))
+}
+
+model_NLR <- function(data, starting_point){
+  model <- optim(par = starting_point, fn = MSE_NLR, data = data)
+  return(list(beta = log(model$par[1]), alpha = model$par[2]))
+}
+
+#Issue i at den her funktion bruger bias adjusted model
 
 bootstrap_loo <- function(model, data, B, alpha) {
   up <- c() ; down <- c() ; pred <- c()
@@ -17,72 +35,101 @@ bootstrap_loo <- function(model, data, B, alpha) {
     fit_mod <- model(data[-i,])
     
     #Computing residuals:
-    log_e <- log(data$Kgp[-i]) - fit_mod$beta - log(data$Sc[-i])*fit_mod$alpha
-    pred <- append(pred, exp(fit_mod$beta + log(data$Sc[i])*fit_mod$alpha)*exp(var(log_e)/2))
+    e <- data$Kgp[-i] / (fit_mod$beta * data$Sc[-i]^fit_mod$alpha)
+    pred <- append(pred, fit_mod$beta * data$Sc[i]^fit_mod$alpha)
     
     #Bootstrapping:
     y_star_n1 <- c()
     for (b in 1:B) {
-      y.star <- exp(fit_mod$beta)*data$Sc[-i]^fit_mod$alpha*exp(sample(log_e))
+      y.star <- fit_mod$beta*data$Sc[-i]^fit_mod$alpha*sample(e)
       boot_data <- data.frame(Sc = data$Sc[-i], Kgp = y.star)
       boot_mod <- model(boot_data)
-      e_boot <- log(y.star) - boot_mod$beta - log(data$Sc[-i])*boot_mod$alpha
-      y_star_n1 <- append(y_star_n1, exp(boot_mod$beta)*data$Sc[i]^boot_mod$alpha*exp(sample(e_boot,1)))
+      e_boot <- y.star / (boot_mod$beta * data$Sc[-i]^boot_mod$alpha)
+      y_star_n1 <- append(y_star_n1, boot_mod$beta*data$Sc[i]^boot_mod$alpha*sample(e_boot,1))
     }
     up <- append(up, quantile(y_star_n1,1-alpha/2)) ; down <- append(down, quantile(y_star_n1,alpha/2))
   }
   return(tibble(High = up, Low = down, Kgp = data$Kgp, Sc = data$Sc, Fitted = pred))
 }
 
-
-#Snak med Dina
-bootstrap <- function(model, train_data, B, alpha, test_data) {
-  up <- c() ; down <- c() ; pred <- c()
-    
-  #Fitting model on training-data:
-  fit_mod <- model(train_data)
-    
-  #Computing residuals:
-  log_e <- log(test_data$Kgp) - fit_mod$beta - log(data$Sc)*fit_mod$alpha
-  pred <-  exp(fit_mod$beta + log(test_data$Sc[i])*fit_mod$alpha)*exp(var(log_e)/2)
-    
-  #Bootstrapping:
-  y_star_n1 <- c()
-  for (b in 1:B) {
-    print(b)
-    y.star <- exp(fit_mod$beta)*train_data$Sc^fit_mod$alpha*exp(sample(log_e))
-    boot_data <- data.frame(Sc = train_data$Sc, Kgp = y.star)
-    boot_mod <- model(boot_data)
-    e_boot <- log(y.star) - boot_mod$beta - log(train_data)*boot_mod$alpha
-    y_boot <- append(y_boot,exp(boot_mod$beta)*test_data$Sc[i]^boot_mod$alpha*exp(sample(e_boot,nrow(test_data))))
-  }
-  up <- append(up, quantile(y_star_n1,1-alpha/2)) ; down <- append(down, quantile(y_star_n1,alpha/2))
-  }
-  return(tibble(High = up, Low = down, Kgp = data$Kgp, Sc = data$Sc, Fitted = pred))
+coverage <- function(pred_int) {
+  mean((pred_int$Low <= pred_int$Kgp) & (pred_int$Kgp <= pred_int$High))
 }
 
-rs_cov_boot <- function(data, k, alpha, pred_int_maker, model, B = 100) {
+#Der er et eller andet som går galt... tænk også på om det er nok at fitte 
+#for hele test i stedet for for hver enkel obs.
+
+rs_cov_boot <- function(data, k, alpha, model, B = 100) {
   cov <- c()
   n <- nrow(data)
   sample_size <- floor(0.8*n)
   
   for (i in (1:k)){
+    print(i)
     # Test and train
     set.seed(7)
     picked_rs <- sample(n,size = sample_size)
     train_rs = data[picked_rs,]
     test_rs = data[-picked_rs,]
     
-    # Fit model
-    model_rs <- pred_int_maker(model, data = train_rs, alpha = alpha, B = B)
-    low <- model_rs[[3]](test_rs$Sc)
-    high <- model_rs[[2]](test_rs$Sc)
+    #Model fit
+    model_rs <- model(train_rs)
+    e <- train_rs$Kgp / (model_rs$beta * train_rs$Sc^model_rs$alpha)
+    pred <- model_rs$beta * test_rs$Sc^model_rs$alpha
+    
+    #Bootstrapping:
+    for (j in 1:nrow(test_rs)) {
+      up <- c() ; down <- c()
+      y_star_n1 <- c()
+      for (b in 1:B) {
+        y.star <- model_rs$beta*train_rs$Sc^model_rs$alpha*sample(e)
+        boot_data <- data.frame(Sc = train_rs$Sc, Kgp = y.star)
+        boot_mod <- model(boot_data)
+        e_boot <- y.star / (boot_mod$beta * train_rs$Sc^boot_mod$alpha)
+        y_star_n1 <- append(y_star_n1, boot_mod$beta*test_rs$Sc[j]^boot_mod$alpha*sample(e_boot,1))
+      }
+      up <- append(up, quantile(y_star_n1,1-alpha/2)) ; down <- append(down, quantile(y_star_n1, alpha/2))
+    }
     
     #Definere
-    cov[i] <- mean(low <= test_rs$Kgp & high >= test_rs$Kgp)
+    cov[i] <- mean((down <= test_rs$Kgp) & (up >= test_rs$Kgp))
   }
   return(tibble("Coverage" = cov))
 }
+
+roll_cov_boot <- function(pred_int, alpha = 0.2, bin_size = 50, title){
+  roll_cov <- c()
+  
+  data_arr <- pred_int %>%
+    arrange(Sc)
+  
+  for (i in seq(1,nrow(data_arr)-bin_size)){
+    data_cov <- data_arr %>%
+      slice(i:(i+bin_size))
+    roll_cov[i] <-mean((data_cov$Low <= data_cov$Kgp) & (data_cov$High >= data_cov$Kgp))
+  }
+  
+  my_tib <- tibble("Bin" = seq(1,nrow(data_arr)-bin_size), "Roll_cov" = roll_cov)
+  
+  up_binom <- qbinom(alpha/2, bin_size, 1-alpha)/bin_size 
+  down_binom <- qbinom(1-alpha/2,bin_size,1-alpha)/bin_size
+  
+  ggplot(my_tib, aes(x = Bin, y = Roll_cov)) + 
+    geom_line(linewidth = 0.6, aes(color = Roll_cov)) + 
+    geom_hline(yintercept = 1-alpha, color = "purple")+
+    geom_hline(yintercept = up_binom, color = "purple", linetype = "dashed", linewidth = 0.3)+
+    geom_hline(yintercept = down_binom, color = "purple", linetype = "dashed", linewidth = 0.3)+
+    theme_bw() +
+    xlab('Sc') + 
+    ylab('Coverage')+
+    labs(title = title)+
+    scale_color_gradient(low = 'blue', high = 'red')
+}
+
+################################################################################
+# Til Gaussiske og lognormale kvantiler
+################################################################################
+
 
 #Creation of prediction intervals
 
@@ -110,10 +157,10 @@ plot_maker <- function(pred_int, title){
   color <- c("in" = "darkolivegreen", "out" = "darkolivegreen3")
   
   ggplot(pred_plot, aes(x = Sc, y = Kgp)) +
-    geom_point(aes(x = Sc, y = Kgp, color = Indicator), size = 2, alpha = 1) + 
-    geom_line(aes(x = Sc, y = High), color = "hotpink", size = 1, alpha = 1) + 
-    geom_line(aes(x = Sc, y = Low), color = "hotpink", size = 1, alpha = 1) +
-    geom_line(aes(x = Sc, y = Fitted), color = "hotpink4", size = 1, alpha = 1) +
+    geom_point(aes(x = Sc, y = Kgp, color = Indicator), size = 0.8, alpha = 0.7) + 
+    geom_point(aes(x = Sc, y = High), color = "hotpink", size = 1, alpha = 0.7) + 
+    geom_point(aes(x = Sc, y = Low), color = "hotpink", size = 1, alpha = 0.7) +
+    geom_line(aes(x = Sc, y = Fitted), color = "hotpink4", size = 1, alpha = 0.7) +
     theme_bw() +
     xlab('Sc') + 
     ylab('Kgp')+
